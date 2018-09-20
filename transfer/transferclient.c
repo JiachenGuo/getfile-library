b4379227255f7a85ec5e92f877b2c9f25d712ef9
Jiachen Guo <guoao1992@gmail.com>
2018-09-20 16:29:45 -0400

    Create transferclient.c

---
+++ transfer/transferclient.c
@@ -0,0 +1,121 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <stdlib.h>
+#include <netdb.h>
+#include <getopt.h>
+
+#define BUFSIZE 2 
+
+#define USAGE                                                \
+    "usage:\n"                                               \
+    "  transferclient [options]\n"                           \
+    "options:\n"                                             \
+    "  -s                  Server (Default: localhost)\n"    \
+    "  -p                  Port (Default: 8803)\n"           \
+    "  -o                  Output file (Default gios.txt)\n" \
+    "  -h                  Show this help message\n"
+
+/* OPTIONS DESCRIPTOR ====================================================== */
+static struct option gLongOptions[] = {
+    {"server", required_argument, NULL, 's'},
+    {"port", required_argument, NULL, 'p'},
+    {"output", required_argument, NULL, 'o'},
+    {"help", no_argument, NULL, 'h'},
+    {NULL, 0, NULL, 0}};
+
+/* Main ========================================================= */
+int main(int argc, char **argv)
+{
+    int option_char = 0;
+    char *hostname = "localhost";
+    unsigned short portno = 8803;
+    char *filename = "gios.txt";
+
+    // Parse and set command line arguments
+    while ((option_char = getopt_long(argc, argv, "s:p:o:h", gLongOptions, NULL)) != -1)
+    {
+        switch (option_char)
+        {
+        default:
+            fprintf(stderr, "%s", USAGE);
+            exit(1);
+        case 's': // server
+            hostname = optarg;
+            break;
+        case 'p': // listen-port
+            portno = atoi(optarg);
+            break;
+        case 'o': // filename
+            filename = optarg;
+            break;
+        case 'h': // help
+            fprintf(stdout, "%s", USAGE);
+            exit(0);
+            break;
+        }
+    }
+
+    if (NULL == filename)
+    {
+        fprintf(stderr, "%s @ %d: invalid filename\n", __FILE__, __LINE__);
+        exit(1);
+    }
+
+    if ((portno < 1025) || (portno > 65535))
+    {
+        fprintf(stderr, "%s @ %d: invalid port number (%d)\n", __FILE__, __LINE__, portno);
+        exit(1);
+    }
+
+    if (NULL == hostname)
+    {
+        fprintf(stderr, "%s @ %d: invalid host name\n", __FILE__, __LINE__);
+        exit(1);
+    }
+
+    /* Socket Code Here */
+    int sockfd, size_recv;
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+	char buffer[BUFSIZE];
+    FILE *fp;
+
+    int total_size = 0;
+
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    server = gethostbyname(hostname);
+
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+
+    bcopy((char *)server->h_addr, 
+         (char *)&serv_addr.sin_addr.s_addr,
+         server->h_length);
+
+    serv_addr.sin_port = htons(portno);
+    connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr));
+
+    fp = fopen(filename, "a+");
+    while(1) {
+        bzero(buffer, BUFSIZE);
+
+        size_recv = recv(sockfd, buffer, BUFSIZE, 0);
+        total_size += size_recv;
+        if (size_recv < 1) {
+            break;
+        } else {
+            fwrite(buffer, 1, size_recv, fp);
+        }
+    }
+
+    printf("total bytes received %d\n", total_size);
+
+    close(sockfd);
+    fclose(fp);
+    return 0;
+}
